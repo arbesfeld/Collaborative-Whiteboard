661123e8920182253330d225a2f9412dfb28fabc
Matthew Arbesfeld <arbesfeld@gmail.com>
2013-11-25 18:46:53 -0500

    Add BoardClientController, we should discuss how to design this stuff

--- src/networking/BoardClientController.java
+++ src/networking/BoardClientController.java
@@ -1,36 +1,41 @@
 package networking;
 
-import game.Board;
-import game.GameState;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.concurrent.BlockingQueue;
 
-import packet.Packet;
+public class BoardClientController implements Runnable {
 
-public class BoardClient implements Runnable {
-    
-    // Communication.
     private BufferedReader in;
     private PrintWriter out;
     private Socket socket;
-    private BlockingQueue<Packet> packetQueue;
+    private BoardClientView view;
     
-    // Board handling.
-    private Board board;
-    private GameState gameState;
-    
-    BoardClient(Socket socket) throws IOException {
-        // TODO: add ui stuff which sends packets to the server
+    BoardClientController(Socket socket, BoardClientView view) throws IOException {
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
         this.socket = socket;
+        this.view = view;
     }
     
+    /**
+     *  Handles the input and output to the client.
+     *  @throws IOException
+     */
+    private void handleConnection() throws IOException {
+        try {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                
+            }
+        } finally {
+            in.close();
+            out.close();
+        }
+    }
+    
+
     /**
      * Start the thread.
      */
@@ -46,21 +51,6 @@ public class BoardClient implements Runnable {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-    }
-    
-    /**
-     *  Handles the input and output to the client.
-     *  @throws IOException
-     */
-    private void handleConnection() throws IOException {
-        try {
-            for (String line = in.readLine(); line != null; line = in.readLine()) {
-                
-            }
-        } finally {
-            in.close();
-            out.close();
         }
     }
 }
